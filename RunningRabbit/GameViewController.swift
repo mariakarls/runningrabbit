18ada789f6f31f34c34912cf76218922b4cd846e
María Karlsdóttir <karlsdottir.maria@gmail.com>
2018-10-11 10:56:13 +0200

    Adding some tutorial links

--- RunningRabbit/GameViewController.swift
+++ RunningRabbit/GameViewController.swift
@@ -10,5 +10,10 @@ import Foundation
 import UIKit
 
 class GameViewController: UIViewController {
+
+    // Hide back button :
+    // https://freakycoder.com/ios-notes-23-how-to-hide-back-button-on-navigationbar-17fb5dfb2b8
     
 }
+
+
--- RunningRabbit/NavigationViewController.swift
+++ RunningRabbit/NavigationViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class NavigationViewController: UIViewController {
 
+    // Image as background
+    // https://stackoverflow.com/questions/27197799/xcode-6-how-to-make-an-image-view-fill-the-screen-on-all-devices-auto-layout-n
+    
+    // Sprite Kit
+    // https://developer.apple.com/spritekit/
+    // https://www.raywenderlich.com/71-spritekit-tutorial-for-beginners
+    
+    
     /*
     override func viewDidLoad() {
         super.viewDidLoad()
