2fb7523853e65bc6946727ae9e6d97f7432d6e59
María Karlsdóttir <karlsdottir.maria@gmail.com>
2018-10-11 08:29:45 +0200

    Initial Commit

---
+++ RunningRabbit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunningRabbit
+//
+//  Created by María Karlsdóttir on 11/10/2018.
+//  Copyright © 2018 María Karlsdóttir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
