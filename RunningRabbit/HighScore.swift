3d7753edc6c2b137713c5692c2b827a44b47640f
María Karlsdóttir <karlsdottir.maria@gmail.com>
2018-10-24 21:22:37 +0200

    Finishing up with high score table for class tomorrow

---
+++ RunningRabbit/HighScore.swift
@@ -0,0 +1,20 @@
+//
+//  HighScore.swift
+//  RunningRabbit
+//
+//  Created by María Karlsdóttir on 24/10/2018.
+//  Copyright © 2018 María Karlsdóttir. All rights reserved.
+//
+
+import Foundation
+
+class HighScore {
+    var score: Int
+    var name: String
+    
+    init(score: Int, name: String) {
+        self.score = score
+        self.name = name
+    }
+    
+}
--- RunningRabbit/HighScoreViewController.swift
+++ RunningRabbit/HighScoreViewController.swift
@@ -9,7 +9,23 @@
 import Foundation
 import UIKit
 
-class HighScoreViewController: UIViewController {
+class HighScoreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Temporary, will later fetch from database
+    var highScores = [
+        HighScore(score: 100, name: "Player 1"),
+        HighScore(score: 95, name: "Player 2"),
+        HighScore(score: 87, name: "Player 3"),
+        HighScore(score: 67, name: "Player 4"),
+        HighScore(score: 44, name: "Player 5"),
+        HighScore(score: 34, name: "Player 6"),
+        HighScore(score: 33, name: "Player 7"),
+        HighScore(score: 30, name: "Player 8"),
+        HighScore(score: 29, name: "Player 9"),
+        HighScore(score: 10, name: "Player 10"),
+        HighScore(score: 9, name: "Player 11"),
+        HighScore(score: 8, name: "Player 12"),
+        HighScore(score: 7, name: "Player 13")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +33,33 @@ class HighScoreViewController: UIViewController {
         // https://pusher.com/tutorials/realtime-table-swift/
         
     }
+    
+    @IBOutlet weak var highScoreTableView: UITableView! {
+        didSet {
+            highScoreTableView.dataSource = self
+            highScoreTableView.delegate = self
+        }
+    }
+    
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "highScoreCell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = String(highScores[indexPath.row].score)
+        cell.detailTextLabel?.text = highScores[indexPath.row].name
+        
+        return cell
+    }
+ 
+
 }
