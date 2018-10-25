742916724a7f6e35ab5f7ce6a82560ea2d02d738
María Karlsdóttir <karlsdottir.maria@gmail.com>
2018-10-25 08:34:09 +0200

    Hide navigation bar on startup view

--- RunningRabbit/GameViewController.swift
+++ RunningRabbit/GameViewController.swift
@@ -14,6 +14,20 @@ class GameViewController: UIViewController {
     // Hide back button :
     // https://freakycoder.com/ios-notes-23-how-to-hide-back-button-on-navigationbar-17fb5dfb2b8
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        //self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        //self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
 }
 
 
--- RunningRabbit/NavigationViewController.swift
+++ RunningRabbit/NavigationViewController.swift
@@ -30,6 +30,21 @@ class NavigationViewController: UIViewController {
     }
  */
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
 
 }
 
